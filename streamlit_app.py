e75ec4768c8f36739b6bd53abdef84a66999afbe
to-afternoon <105660831+to-afternoon@users.noreply.github.com>
2022-05-17 00:26:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # %matplotlib inline
 import io
 from sklearn import preprocessing
