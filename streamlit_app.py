af1719bc28e1316b3a6f0ad539d9ddf9c6ba1187
to-afternoon <105660831+to-afternoon@users.noreply.github.com>
2022-05-17 00:24:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
+# %matplotlib inline
 import io
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
